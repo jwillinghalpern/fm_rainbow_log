16359d9383444fc254d7b326cfe4d8c79e0bd5c7
Joshua Willing <jwillinghalpern@gmail.com>
2023-04-28 01:55:22 -0700

    working. add clap.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
+use clap::Parser;
 use colored::Colorize;
 // TODO: migrate notify to v5?
 use iso8601::parsers::parse_datetime;
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::{Read, Seek, SeekFrom};
 use std::sync::mpsc;
 use std::time::Duration;
+
+#[derive(Parser, Debug)]
+pub struct Config {
+    #[arg(help = "The file to watch, should probably be path/to/Import.log")]
+    file: String,
+}
+
+fn replace_trailing_cr_with_crlf(buf: &mut String) {
+    let mut prev = 0;
+    let mut new_buf = String::new();
+    // I ended up having to use buf.chars() instead of buf.bytes() to preserve smart quotes, ugh
+    for c in buf.chars() {
+        let byte = c as u8;
+        if prev == 13 && byte != 10 {
+            new_buf.push(10 as char);
+        }
+        new_buf.push(c);
+        prev = byte;
+    }
+    *buf = new_buf;
+}
 
 fn is_timestamp(s: &str) -> bool {
     let s = s.replace(" ", "T");
     parse_datetime(s.as_bytes()).is_ok()
 }
+
 fn print_line(line: &str) {
+    // println!("{}", line);
+
     let v = line.splitn(4, '\t').collect::<Vec<&str>>();
     let timestamp = *v.get(0).unwrap_or(&"");
     let filename = *v.get(1).unwrap_or(&"");
@@ -19,17 +44,20 @@ fn print_line(line: &str) {
     let message = *v.get(3).unwrap_or(&"");
 
     let colored_line = if !is_timestamp(timestamp) {
-        println!("found non-timestamp line");
+        // capture extra lines after an error, like when a bad multiline calc is printed, and print them red too
+        // this logic assumes that the timestamp is always the first column in non-error lines
         format!("{}", line.red())
     } else if code != "0" {
+        // color errors differently
         format!(
-            "> {}\t{}\t{}\t{}",
-            timestamp.bright_red(),
+            "{}\t{}\t{}\t{}",
+            timestamp.bright_white().on_red(),
             filename.bright_red(),
             code.bright_white().on_red(),
             message.red()
         )
     } else {
+        // regular log msg
         format!(
             "{}\t{}\t{}\t{}",
             timestamp.green(),
@@ -37,47 +65,42 @@ fn print_line(line: &str) {
             code.yellow(),
             message.blue()
         )
-        // "".to_string()
     };
 
     println!("{}", colored_line);
 }
 
 fn main() {
-    // let s = "2023-04-27 20:50:10.648 -0700";
-    // println!("{}", is_timestamp(s));
-    // std::process::exit(1);
-
-    // TODO: use clap crate to specify input and help
-    let path = std::env::args().nth(1).unwrap();
+    let config = Config::parse();
+    let path = config.file;
 
     let (tx, rx) = mpsc::channel();
     let mut watcher = watcher(tx, Duration::from_millis(100)).unwrap();
     watcher.watch(&path, RecursiveMode::NonRecursive).unwrap();
 
-    let mut contents = fs::read_to_string(&path).unwrap();
-    let mut pos = contents.len() as u64;
-
-    // TODO: print these in color
-    // TODO: consider only printing the last few lines
-    // print!("{}", contents);
-    // contents.lines().for_each(|line| print_line(line));
-    contents.lines().for_each(|line| println!("{}", line));
+    let file = File::open(&path).unwrap();
+    let mut reader = std::io::BufReader::new(&file);
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf).unwrap();
+    replace_trailing_cr_with_crlf(&mut buf);
+    let mut pos = buf.len() as u64;
+    buf.lines().for_each(|line| print_line(line));
 
     loop {
         match rx.recv() {
             Ok(DebouncedEvent::Write(_)) => {
-                let mut f = File::open(&path).unwrap();
-                f.seek(SeekFrom::Start(pos)).unwrap();
+                // let file = File::open(&path).unwrap();
+                // let mut reader = std::io::BufReader::new(&file);
+                // // only read new data since last read
+                reader.seek(SeekFrom::Start(pos)).unwrap();
 
-                pos = f.metadata().unwrap().len();
+                // move pos to end
+                pos = file.metadata().unwrap().len();
 
-                contents.clear();
-                f.read_to_string(&mut contents).unwrap();
-                contents
-                    .lines()
-                    .into_iter()
-                    .for_each(|line| print_line(line));
+                buf.clear();
+                reader.read_to_string(&mut buf).unwrap();
+                replace_trailing_cr_with_crlf(&mut buf);
+                buf.lines().for_each(|line| print_line(line));
             }
             Ok(_) => {}
             Err(err) => {
