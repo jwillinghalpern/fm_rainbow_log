f082b54033d6c58a29e5ebf0c1c6a1f554934a90
Joshua Willing <jwillinghalpern@gmail.com>
2023-04-28 19:37:24 -0700

    rename vars

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,16 @@
-// TODO: migrate notify to iso8601 v5?
 use iso8601::parsers::parse_datetime;
 
 pub(crate) fn replace_trailing_cr_with_crlf(buf: &mut String) {
-    let mut prev = 0;
+    let mut prev_byte = 0;
     let mut new_buf = String::new();
-    // I ended up having to use buf.chars() instead of buf.bytes() to preserve smart quotes, ugh
+    // I ended up having to use buf.chars() instead of buf.bytes() to preserve "smart" quotes, ugh
     for c in buf.chars() {
-        let byte = c as u8;
-        if prev == 13 && byte != 10 {
+        let curr_byte = c as u8;
+        if prev_byte == 13 && curr_byte != 10 {
             new_buf.push(10 as char);
         }
         new_buf.push(c);
-        prev = byte;
+        prev_byte = curr_byte;
     }
     *buf = new_buf;
 }
