617350722c0eac5f9aa85781df22e1622ad4350f
Joshua Willing <jwillinghalpern@gmail.com>
2023-06-08 13:44:53 -0700

    Fixes #23: Squashed commit of the following. properly waits child process. Also conditional noop beep for windows.
    commit 6e015cff99d9a4ff78ec0ad805a742836791d601
    Author: Josh Willing <jwillinghalpern@gmail.com>
    Date:   Thu Jun 8 13:30:20 2023 -0700
    
        conditionally compile noop beep for windows
    
    commit b6f28168143e2c7c1da0e555b3d70830b330a7cb
    Author: Joshua Willing <jwillinghalpern@gmail.com>
    Date:   Thu Jun 8 13:13:15 2023 -0700
    
        waits afplay child process

--- src/beeper.rs
+++ src/beeper.rs
@@ -1,3 +1,4 @@
+#[cfg(target_os = "macos")]
 pub fn beep(path: &str, volume: f32) {
     let vol = if !(0.0..=1.0).contains(&volume) {
         1.0
@@ -9,9 +10,24 @@ pub fn beep(path: &str, volume: f32) {
     } else {
         path
     };
-    let _ = std::process::Command::new("afplay")
+    if let Ok(mut child) = std::process::Command::new("afplay")
         .arg("-v")
         .arg(vol.to_string())
         .arg(path)
-        .spawn();
+        .spawn()
+    {
+        // previx underscore to avoid conditional compilation warning in release build.
+        // _res is only used in debug mode.
+        let _res = child.wait();
+        #[cfg(debug_assertions)]
+        if let Err(e) = _res {
+            eprintln!("afplay wait error: {}", e);
+        };
+    } else {
+        #[cfg(debug_assertions)]
+        eprintln!("afplay not found");
+    }
 }
+
+#[cfg(target_os = "windows")]
+pub fn beep(_path: &str, _volume: f32) {}
